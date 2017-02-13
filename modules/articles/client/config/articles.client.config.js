19f9c2f3a6c2c328c538dfe4820e55ccace1ab7a
novotny <novotny@ufl.edu>
2017-02-12 22:06:43 -0700

    added create article option to main nav bar

--- modules/articles/client/config/articles.client.config.js
+++ modules/articles/client/config/articles.client.config.js
@@ -11,17 +11,19 @@ angular.module('articles').run(['Menus',
       roles: ['*']
     });
 
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'articles', {
+      title: 'Create Articles',
+      state: 'articles.create',
+      //roles: ['user']
+    });
+
     // Add the dropdown list item
     Menus.addSubMenuItem('topbar', 'articles', {
       title: 'List Articles',
       state: 'articles.list'
     });
 
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'Create Articles',
-      state: 'articles.create',
-      roles: ['user']
-    });
+
   }
 ]);
