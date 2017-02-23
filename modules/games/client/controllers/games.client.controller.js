96a2adc966385e3437cd3b70d79d54ec7adecb74
Nuchaz <nuchaz.ipod@gmail.com>
2017-02-22 22:25:52 -0500

    The word search game is more or less functional now
    Now the admin (only admin) can create a new game, select word search and fill in the form and the word search game will be generated. It seems to be saving and loading from the database with no issues at this point. You can click/touch 2 points to select a letter range and if found, I mark the answer as found. It still needs work to make the play experience more smooth, i.e. automatically deselecting words when clicking somewhere else on the board and leaving the word you found crossed on the game board. I also would like to indicate the player is a "winner" if they find all the words.

--- modules/games/client/controllers/games.client.controller.js
+++ modules/games/client/controllers/games.client.controller.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-
+    // word search variables
     var canvas;
     var tile;
     var selectionOne = {
@@ -16,25 +16,8 @@
         letter: "A"
     }
     var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    var testLetter = ['B','A','C','O','N','M','A','H']
     var gameboard = new Array(15);
-    for (var i =0; i < 15; i++) {
-        gameboard[i] = new Array(15);
-        for (var j = 0; j < 15; j++){
-            var rando = Math.random() * 26;
-            var gamePiece = {letter:letters[Math.floor(rando)], xCoord:0, yCoord:0, isSelected:false};
-            if (i === 0 && j < testLetter.length){
-              gamePiece.letter = testLetter[j];
-            }
-            gameboard[i][j] = gamePiece;
-        }
-    }
-    var answerStrings = ['bacon','steak','cheese','ribs','ham','chicken','salad','potato','mushroom','pepperoni','sausage','bbq','bread','lettuce','carrot','beans','food'];
-    var answers = new Array(answerStrings.length);
-    for (var i = 0; i < answerStrings.length; i++){
-      var newanswer = {string:answerStrings[i], found:false, selectionFirst:selectionOne, selectionSecond:selectionTwo};
-      answers[i] = newanswer;
-    }
+    var answers;
 
   // Games controller
   angular
@@ -45,14 +28,21 @@
 
   function GamesController ($scope, $state, $window, Authentication, game) {
     var vm = this;
-
     vm.authentication = Authentication;
     vm.game = game;
     vm.error = null;
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
-    init(); // initialize the game board
+
+    if (game.name != undefined){
+      parseAnswers(vm);
+      parseGameboard(vm);
+      init();// initialize the game board, this should only fire if the wordsearch gameveiw has been loaded
+
+    }
+    $scope.gameTypes = ["Wordsearch", "Maze", "Test"];
+
     // Remove existing Game
     function remove() {
       if ($window.confirm('Are you sure you want to delete?')) {
@@ -85,208 +75,260 @@
       }
     }
 
-      $scope.clicked = function(event){
-        // eventually I will have to pass the clicked event on to the function that is based on whatever game we are going to run.
-        // I might even be wise to get the games running in a seperate javascript file
-        var address = detectClickAddress(event.offsetX, event.offsetY);
-        checkSelection(address);
-        var foundString = getTextString();
-        if (foundString.length > 0){
-          // when a string is found, I need to leave the line / circle around the found word so it remains rendering.
-          checkStringIsAnswer(foundString);
-        }
-        draw();
+    $scope.dropDownChange = function(event){
+
+    }
+
+    $scope.clicked = function(event){
+      // eventually I will have to pass the clicked event on to the function that is based on whatever game we are going to run.
+      // I might even be wise to get the games running in a seperate javascript file
+      var address = detectClickAddress(event.offsetX, event.offsetY);
+      checkSelection(address);
+      var foundString = getTextString();
+      if (foundString.length > 0){
+        // when a string is found, I need to leave the line / circle around the found word so it remains rendering.
+        checkStringIsAnswer(foundString);
+      }
+      draw();
+    }
+  }
+
+  function init() {
+    canvas = document.getElementById("gameCanvas");
+    tile = canvas.getContext("2d");
+    draw();
+  }
+
+  function parseAnswers(vm){
+      var answerList = vm.game.answerLine
+      answerList = answerList.replace(/\s+/g, '');
+      answerList = answerList.split(",")
+      answers = new Array(answerList.length);
+      for (var i = 0; i < answerList.length; i++){
+          answers[i] = {string:answerList[i], found:false, selectionFirst:selectionOne, selectionSecond:selectionTwo};
       }
   }
 
-    function init() {
-      canvas = document.getElementById("gameCanvas");
-      tile = canvas.getContext("2d");
-      draw();
+  function parseGameboard(vm){
+    for (var i = 0; i < gameboard.length; i++){
+      gameboard[i] = new Array(15);
     }
+    parseGameboardLine(vm.game.line1, 0);
+    parseGameboardLine(vm.game.line2, 1);
+    parseGameboardLine(vm.game.line3, 2);
+    parseGameboardLine(vm.game.line4, 3);
+    parseGameboardLine(vm.game.line5, 4);
+    parseGameboardLine(vm.game.line6, 5);
+    parseGameboardLine(vm.game.line7, 6);
+    parseGameboardLine(vm.game.line8, 7);
+    parseGameboardLine(vm.game.line9, 8);
+    parseGameboardLine(vm.game.line10, 9);
+    parseGameboardLine(vm.game.line11, 10);
+    parseGameboardLine(vm.game.line12, 11);
+    parseGameboardLine(vm.game.line13, 12);
+    parseGameboardLine(vm.game.line14, 13);
+    parseGameboardLine(vm.game.line15, 14);
+  }
 
-    /* Calls all the draw functions */
-    function draw(){
-      tile.clearRect(0, 0, canvas.width, canvas.height);
-      drawBoard();
-      drawAnswers();
-      drawSelection();
-    }
-
-    /* Draws the selection arounnd the letter(s) */
-    function drawSelection(){
-      if (selectionOne.found){
-        tile.beginPath();
-        tile.arc(25 + selectionOne.xCoord * 32, 17 + selectionOne.yCoord * 32, 12, 0, Math.PI*2, false);
-        tile.strokeStyle = "#FF0000";
-        tile.stroke();
-        tile.closePath();
+  function parseGameboardLine(line, boardj){
+      line = line.replace(/\s+/g, ''); // remove all the empty space
+      line = line.replace(',', ''); // remove any commas
+      line = line.toUpperCase(); // make all caps
+      for (var i = 0; i < 15; i++){
+          if (line.length >= i){
+              var gamePiece = {letter:line[i], xCoord:0, yCoord:0, isSelected:false};
+              gameboard[i][boardj] = gamePiece;
+          } else {
+              // The line that was saved is short, add in random letters to make up the difference
+              var rando = Math.random() * 26;
+              var gamePiece = {letter:letters[Math.floor(rando)], xCoord:0, yCoord:0, isSelected:false};
+              gameboard[i][boardj] = gamePiece;
+          }
       }
+  }
 
-      if (selectionTwo.found){
-          tile.beginPath();
-        tile.arc(25 + selectionTwo.xCoord * 32, 17 + selectionTwo.yCoord * 32, 12, 0, Math.PI*2, false);
-        tile.strokeStyle = "#FF0000";
-        tile.stroke();
-        tile.closePath();
+  /* Calls all the draw functions */
+  function draw(){
+    tile.clearRect(0, 0, canvas.width, canvas.height);
+    drawBoard();
+    drawAnswers();
+    drawSelection();
+  }
 
-        tile.beginPath();
-        tile.strokeStyle = "#FFFF00";
-
-        tile.moveTo(25 + selectionOne.xCoord * 32, 17 + selectionOne.yCoord * 32);
-        tile.lineTo(25 + selectionTwo.xCoord * 32, 17 + selectionTwo.yCoord * 32);
-        tile.stroke();
-        tile.closePath();
-      }
-    }
-
-     /* Draws the game board in the form of a 15x15 word search game board */
-    function drawBoard(){
-      for (var i = 0; i < gameboard.length; i++){
-        for (var j = 0; j < gameboard.length; j++){
-          var xCoord = 15 + i * 32;
-          var yCoord = 25 + j * 32;
-          gameboard[i][j].xCoord = xCoord;
-          gameboard[i][j].yCoord = yCoord;
-          tile.beginPath();
-          tile.fillStyle = "#000000";
-          tile.font = '24px serif';
-          tile.fillText(gameboard[i][j].letter, xCoord, yCoord);
-          tile.closePath();
-        }
-      }
-    }
-     /* draws the answers at the bottom of the game board */
-    function drawAnswers(){
+  /* Draws the selection arounnd the letter(s) */
+  function drawSelection(){
+    if (selectionOne.found){
       tile.beginPath();
-      for (var i = 0; i < answers.length; i++) {
-        if (answers[i].found){
-          tile.fillStyle = "#FF0000";
-        } else {
-          tile.fillStyle = "#000000";
-        }
-        tile.font = '24px serif';
-        if (i < 6) {
-            tile.fillText(answers[i].string, 25, 524 + i * 28);
-        } else if (i < 12) {
-            tile.fillText(answers[i].string, 205, 524 + ((i - 6) * 28));
-        } else {
-            tile.fillText(answers[i].string, 375, 524 + ((i - 12) * 28));
-        }
-      }
+      tile.arc(25 + selectionOne.xCoord * 32, 17 + selectionOne.yCoord * 32, 12, 0, Math.PI*2, false);
+      tile.strokeStyle = "#FF0000";
+      tile.stroke();
       tile.closePath();
     }
 
+    if (selectionTwo.found){
+        tile.beginPath();
+      tile.arc(25 + selectionTwo.xCoord * 32, 17 + selectionTwo.yCoord * 32, 12, 0, Math.PI*2, false);
+      tile.strokeStyle = "#FF0000";
+      tile.stroke();
+      tile.closePath();
+
+      tile.beginPath();
+      tile.strokeStyle = "#FFFF00";
+
+      tile.moveTo(25 + selectionOne.xCoord * 32, 17 + selectionOne.yCoord * 32);
+      tile.lineTo(25 + selectionTwo.xCoord * 32, 17 + selectionTwo.yCoord * 32);
+      tile.stroke();
+      tile.closePath();
+    }
+  }
+
+   /* Draws the game board in the form of a 15x15 word search game board */
+  function drawBoard(){
+    for (var i = 0; i < gameboard.length; i++){
+      for (var j = 0; j < gameboard.length; j++){
+        var xCoord = 15 + i * 32;
+        var yCoord = 25 + j * 32;
+        gameboard[i][j].xCoord = xCoord;
+        gameboard[i][j].yCoord = yCoord;
+        tile.beginPath();
+        tile.fillStyle = "#000000";
+        tile.font = '24px serif';
+        tile.fillText(gameboard[i][j].letter, xCoord, yCoord);
+        tile.closePath();
+      }
+    }
+  }
+   /* draws the answers at the bottom of the game board */
+  function drawAnswers(){
+    tile.beginPath();
+    for (var i = 0; i < answers.length; i++) {
+      if (answers[i].found){
+        tile.fillStyle = "#FF0000";
+      } else {
+        tile.fillStyle = "#000000";
+      }
+      tile.font = '24px serif';
+      if (i < 6) {
+          tile.fillText(answers[i].string, 25, 524 + i * 28);
+      } else if (i < 12) {
+          tile.fillText(answers[i].string, 205, 524 + ((i - 6) * 28));
+      } else {
+          tile.fillText(answers[i].string, 375, 524 + ((i - 12) * 28));
+      }
+    }
+    tile.closePath();
+  }
+
   /* Checks if the click address has selected the first or second letter */
-    function checkSelection(address){
-      var i = address.iAddress;
-      var j = address.jAddress;
-      if (selectionOne.found){
-        if (address.found){
-          if ((selectionOne.xCoord === i && selectionOne.yCoord === j) || (selectionTwo.found && selectionTwo.xCoord === i && selectionTwo.yCoord === j)){
-            selectionOne.found = false;
-            selectionTwo.found = false;
-          } else {
-            var compatible = detectCompatibleSelection(address);
-            if (compatible){
-              selectionTwo.xCoord = i;
-              selectionTwo.yCoord = j;
-              selectionTwo.letter = address.letter;
-              selectionTwo.found = true;
-            }
-            // Test if the address found is compatible with selectionOne. In other words, is it on the same x OR y OR is it diagonal. Apply selectionTwo if so
-            // maybe I should found = false on selection one if incompatible thing?
+  function checkSelection(address){
+    var i = address.iAddress;
+    var j = address.jAddress;
+    if (selectionOne.found){
+      if (address.found){
+        if ((selectionOne.xCoord === i && selectionOne.yCoord === j) || (selectionTwo.found && selectionTwo.xCoord === i && selectionTwo.yCoord === j)){
+          selectionOne.found = false;
+          selectionTwo.found = false;
+        } else {
+          var compatible = detectCompatibleSelection(address);
+          if (compatible){
+            selectionTwo.xCoord = i;
+            selectionTwo.yCoord = j;
+            selectionTwo.letter = address.letter;
+            selectionTwo.found = true;
+          }
+          // Test if the address found is compatible with selectionOne. In other words, is it on the same x OR y OR is it diagonal. Apply selectionTwo if so
+          // maybe I should found = false on selection one if incompatible thing?
+        }
+      }
+    } else {
+      if (address.found){
+        selectionOne.xCoord = i;
+        selectionOne.yCoord = j;
+        selectionOne.letter = address.letter;
+        selectionOne.found = true;
+      }
+    }
+  }
+
+  /* Check if string is an answer */
+  function checkStringIsAnswer(string){
+    var reverseString = "";
+    for (var i = string.length - 1; i >= 0; i--) {
+      reverseString += string[i];
+    }
+    reverseString = reverseString.toString().toLowerCase();
+    for (var i = 0; i < answers.length; i++) {
+      var stringTest = string.toString().toLowerCase();
+      var answerTest = answers[i].string.toString().toLowerCase();
+      if (answerTest.valueOf() === stringTest.valueOf() || answerTest.valueOf() === reverseString.valueOf()){
+        answers[i].found = true;
+        console.log("Found answer: " + answers[i].string);
+      }
+    }
+  }
+
+  /* Checks the two selection points and gets returns a string between the points */
+  function getTextString(){
+    // I am betting this function can be optimizted in some clever way.
+    var returnValue = "";
+    if (selectionOne.found && selectionTwo.found){
+      if (selectionOne.xCoord === selectionTwo.xCoord) {
+        //vertical
+        if (selectionOne.yCoord > selectionTwo.yCoord){
+            //selectionTwo is first, reading left to right
+          for (var i = selectionTwo.yCoord; i <= selectionOne.yCoord; i++){
+            returnValue += gameboard[selectionOne.xCoord][i].letter;
+          }
+        } else {
+            //selectionOne is first, reading left to right
+          for (var i = selectionOne.yCoord; i <= selectionTwo.yCoord; i++){
+            returnValue += gameboard[selectionOne.xCoord][i].letter;
+          }
+        }
+      } else if (selectionOne.yCoord === selectionTwo.yCoord) {
+        // horizontal
+        if (selectionOne.xCoord > selectionTwo.xCoord){
+          for (var i = selectionTwo.xCoord; i <= selectionOne.xCoord; i++){
+            returnValue += gameboard[i][selectionOne.yCoord].letter;
+          }
+        } else {
+          for (var i = selectionOne.xCoord; i <= selectionTwo.xCoord; i++){
+            returnValue += gameboard[i][selectionOne.yCoord].letter;
           }
         }
       } else {
-        if (address.found){
-          selectionOne.xCoord = i;
-          selectionOne.yCoord = j;
-          selectionOne.letter = address.letter;
-          selectionOne.found = true;
-        }
-      }
-    }
-
-    /* Check if string is an answer */
-    function checkStringIsAnswer(string){
-      var reverseString = "";
-      for (var i = string.length - 1; i >= 0; i--) {
-        reverseString += string[i];
-      }
-      reverseString = reverseString.toString().toLowerCase();
-      for (var i = 0; i < answers.length; i++) {
-        var stringTest = string.toString().toLowerCase();
-        var answerTest = answers[i].string.toString().toLowerCase();
-        if (answerTest.valueOf() === stringTest.valueOf() || answerTest.valueOf() === reverseString.valueOf()){
-          answers[i].found = true;
-          console.log("Found answer: " + answers[i].string);
-        }
-      }
-    }
-
-    /* Checks the two selection points and gets returns a string between the points */
-    function getTextString(){
-      // I am betting this function can be optimizted in some clever way.
-      var returnValue = "";
-      if (selectionOne.found && selectionTwo.found){
-        if (selectionOne.xCoord === selectionTwo.xCoord) {
-          //vertical
+        //diagonal
+        if (selectionOne.xCoord > selectionTwo.xCoord) {
+          //selectionTwo is first, going left to right
           if (selectionOne.yCoord > selectionTwo.yCoord){
-              //selectionTwo is first, reading left to right
-            for (var i = selectionTwo.yCoord; i <= selectionOne.yCoord; i++){
-              returnValue += gameboard[selectionOne.xCoord][i].letter;
+          // going left to right, bottom to top
+            for (var i = selectionTwo.xCoord, j = selectionTwo.yCoord; i <= selectionOne.xCoord; i++, j++){
+              returnValue += gameboard[i][j].letter;
             }
           } else {
-              //selectionOne is first, reading left to right
-            for (var i = selectionOne.yCoord; i <= selectionTwo.yCoord; i++){
-              returnValue += gameboard[selectionOne.xCoord][i].letter;
-            }
-          }
-        } else if (selectionOne.yCoord === selectionTwo.yCoord) {
-          // horizontal
-          if (selectionOne.xCoord > selectionTwo.xCoord){
-            for (var i = selectionTwo.xCoord; i <= selectionOne.xCoord; i++){
-              returnValue += gameboard[i][selectionOne.yCoord].letter;
-            }
-          } else {
-            for (var i = selectionOne.xCoord; i <= selectionTwo.xCoord; i++){
-              returnValue += gameboard[i][selectionOne.yCoord].letter;
+          // doing left to right, top to bottom
+            for (var i = selectionTwo.xCoord, j = selectionTwo.yCoord; i <= selectionOne.xCoord; i++, j--){
+                returnValue += gameboard[i][j].letter;
             }
           }
         } else {
-          //diagonal
-          if (selectionOne.xCoord > selectionTwo.xCoord) {
-            //selectionTwo is first, going left to right
-            if (selectionOne.yCoord > selectionTwo.yCoord){
+          //selectionOne is first, going left to right
+          if (selectionTwo.yCoord > selectionOne.yCoord){
             // going left to right, bottom to top
-              for (var i = selectionTwo.xCoord, j = selectionTwo.yCoord; i <= selectionOne.xCoord; i++, j++){
+            for (var i = selectionOne.xCoord, j = selectionOne.yCoord; i <= selectionTwo.xCoord; i++, j++){
                 returnValue += gameboard[i][j].letter;
-              }
-            } else {
-            // doing left to right, top to bottom
-              for (var i = selectionTwo.xCoord, j = selectionTwo.yCoord; i <= selectionOne.xCoord; i++, j--){
-                  returnValue += gameboard[i][j].letter;
-              }
             }
           } else {
-            //selectionOne is first, going left to right
-            if (selectionTwo.yCoord > selectionOne.yCoord){
-              // going left to right, bottom to top
-              for (var i = selectionOne.xCoord, j = selectionOne.yCoord; i <= selectionTwo.xCoord; i++, j++){
-                  returnValue += gameboard[i][j].letter;
-              }
-            } else {
-              // doing left to right, top to bottom
-              for (var i = selectionOne.xCoord, j = selectionOne.yCoord; i <= selectionTwo.xCoord; i++, j--){
-                  returnValue += gameboard[i][j].letter;
-              }
+            // doing left to right, top to bottom
+            for (var i = selectionOne.xCoord, j = selectionOne.yCoord; i <= selectionTwo.xCoord; i++, j--){
+                returnValue += gameboard[i][j].letter;
             }
           }
         }
       }
-      return returnValue;
     }
+    return returnValue;
+  }
 
   /* Checks if a selection range is valid */
     function detectCompatibleSelection(secondSel){
