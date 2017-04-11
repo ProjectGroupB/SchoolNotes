3846b8459eb6b65a89c5e5963067b161425260c4
novotny <novotny@ufl.edu>
2017-04-11 07:18:34 -0600

    removed profile image from comments section; fixed comment submission

--- modules/articles/client/controllers/articles.client.controller.js
+++ modules/articles/client/controllers/articles.client.controller.js
@@ -45,7 +45,7 @@ app.controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Au
     };
 
     // add comments
-    $scope.addComment = function () {
+    $scope.addComment = function (article) {
 
       // time
       var today = new Date();
@@ -101,44 +101,44 @@ app.controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Au
     };
 
 
-    $scope.comment = function (article) {
-
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
-
-      if(dd<10) {
-        dd='0'+dd;
-      }
-
-      if(mm<10) {
-        mm='0'+mm;
-      }
-
-      today = mm+'/'+dd+'/'+yyyy;
-
-      var comments = 'no comments';
-
-      if (article) {
-        comments = article.comments;
-        comments = comments + ' - ' + $scope.authentication.user.displayName + ' - ';
-        comments = comments + ' [' + today + '] ' + this.comments + '\r\n';
-        article.$update();
-        $scope.comments = '';
-
-      }
-      else {
-        $scope.article.comments = [newComment];
-        $scope.article.$update(function () {
-          $location.path('articles/' + $scope.article._id + '/review');
-        }, function (errorResponse) {
-          $scope.error = errorResponse.data.message;
-        });
-        $scope.commentTitle = '';
-        $scope.commentDetails = '';
-      }
-    };
+    // $scope.comment = function (article) {
+    //
+    //   var today = new Date();
+    //   var dd = today.getDate();
+    //   var mm = today.getMonth()+1; //January is 0!
+    //   var yyyy = today.getFullYear();
+    //
+    //   if(dd<10) {
+    //     dd='0'+dd;
+    //   }
+    //
+    //   if(mm<10) {
+    //     mm='0'+mm;
+    //   }
+    //
+    //   today = mm+'/'+dd+'/'+yyyy;
+    //
+    //   var comments = 'no comments';
+    //
+    //   if (article) {
+    //     comments = article.comments;
+    //     comments = comments + ' - ' + $scope.authentication.user.displayName + ' - ';
+    //     comments = comments + ' [' + today + '] ' + this.comments + '\r\n';
+    //     article.$update();
+    //     $scope.comments = '';
+    //
+    //   }
+    //   else {
+    //     $scope.article.comments = [newComment];
+    //     $scope.article.$update(function () {
+    //       $location.path('articles/' + $scope.article._id + '/review');
+    //     }, function (errorResponse) {
+    //       $scope.error = errorResponse.data.message;
+    //     });
+    //     $scope.commentTitle = '';
+    //     $scope.commentDetails = '';
+    //   }
+    // };
 
     //
     // $scope.comment = function (article) {
