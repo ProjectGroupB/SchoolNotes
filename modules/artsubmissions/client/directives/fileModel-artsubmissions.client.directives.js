137a1faf55d6954b2b74fe624708fb538917acf5
morlando18 <morlando18@gmail.com>
2017-04-17 16:04:01 -0400

    Cleaning up art submission module

--- modules/artsubmissions/client/directives/fileModel-artsubmissions.client.directives.js
+++ modules/artsubmissions/client/directives/fileModel-artsubmissions.client.directives.js
@@ -17,22 +17,3 @@ angular.module('fileModelDirective', [])
       }
     };
   }]);
-
-
-// angular.module('fileModelDirective', [])
-//
-// .directive('fileModel', ['$parse', function($parse){
-//   return{
-//     restrict:'A',
-//     link: function(scope, element, attrs) {
-//       var parsedFile = $parse(attrs.fileModel);
-//       var parsedFileSetter = parsedFile.assign;
-//
-//       element.bind('change', function() {
-//         scope.$apply(function () {
-//           parsedFileSetter(scope, element[0].files[0]);
-//         });
-//       });
-//     }
-//   };
-// }]);
