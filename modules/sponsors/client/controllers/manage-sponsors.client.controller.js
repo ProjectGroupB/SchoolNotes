905ebc5df346c45e266bbec59b19237429a247e3
Nuchaz <nuchaz.ipod@gmail.com>
2017-04-18 00:57:39 -0400

    fixed some grunt errors relating to formatting

--- modules/sponsors/client/controllers/manage-sponsors.client.controller.js
+++ modules/sponsors/client/controllers/manage-sponsors.client.controller.js
@@ -1,40 +1,40 @@
 
-    'use strict';
+'use strict';
 
-    angular.module('sponsors').controller('SponsorsManageController', SponsorsManageController);
+angular.module('sponsors').controller('SponsorsManageController', SponsorsManageController);
 
-    SponsorsManageController.$inject = ['$scope','SponsorsService'];
+SponsorsManageController.$inject = ['$scope','SponsorsService'];
 
-    function SponsorsManageController($scope, SponsorsService, thumbnail, leftAd, rightAd) {
-        $scope.sponsors = SponsorsService.query();
-        $scope.thumbnail = thumbnail;
-        $scope.leftAd = leftAd;
-        $scope.rightAd = rightAd;
+function SponsorsManageController($scope, SponsorsService, thumbnail, leftAd, rightAd) {
+  $scope.sponsors = SponsorsService.query();
+  $scope.thumbnail = thumbnail;
+  $scope.leftAd = leftAd;
+  $scope.rightAd = rightAd;
 
-        $scope.makeAd = function(sponsor){
-            document.getElementById("adModal").style.display = "block";
-            $scope.thumbnail = sponsor.thumbnail;
-        };
+  $scope.makeAd = function(sponsor){
+    document.getElementById('adModal').style.display = 'block';
+    $scope.thumbnail = sponsor.thumbnail;
+  };
 
-        // assign ad to the proper ad space in api
-        $scope.left = function() {
-            $scope.leftAd = $scope.thumbnail;
-        };
+  // assign ad to the proper ad space in api
+  $scope.left = function() {
+    $scope.leftAd = $scope.thumbnail;
+  };
 
-        $scope.right = function() {
-            $scope.rightAd = $scope.thumbnail;
-        };
+  $scope.right = function() {
+    $scope.rightAd = $scope.thumbnail;
+  };
 
-        document.getElementsByClassName("close")[0].onclick = function() {
-            document.getElementById("adModal").style.display = "none";
-        };
+  document.getElementsByClassName('close')[0].onclick = function() {
+    document.getElementById('adModal').style.display = 'none';
+  };
 
-        window.onclick = function(event) {
-            if (event.target === document.getElementById("adModal")) {
-                document.getElementById("adModal").style.display = "none";
-            }
-        };
-
+  window.onclick = function(event) {
+    if (event.target === document.getElementById('adModal')) {
+      document.getElementById('adModal').style.display = 'none';
     }
+  };
+
+}
 
 
